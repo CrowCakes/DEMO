ef9c14070c845d6d25cf1689a0094184a7b35a35
CrowCakes <jeremychoa@gmail.com>
2019-06-18 17:14:26 +0800

    Update FilterOrder.sql
    Fixed query so that it now also checks Units without Parts and Units with Parts

--- queries/FilterOrder.sql
+++ queries/FilterOrder.sql
@@ -1,4 +1,16 @@
-SELECT A.OrderID, A.Client, A.RFD, A.ARD, A.AccountManager, A.PO, A.RR, A.RTS, A.Status,
+(SELECT A.OrderID, A.Client, A.RFD, A.ARD, A.AccountManager, A.PO, A.RR, A.RTS, A.Status,
+B.ListID, B.DateOrdered, B.DateDue, B.Remarks
+FROM DemoOrder as A 
+INNER JOIN DemoDuration as B
+ON A.OrderID = B.OrderID
+INNER JOIN DemoOrderUnits as C
+ON A.OrderID = C.OrderID
+INNER JOIN Items as D
+ON C.ItemID = D.ItemID
+WHERE D.Serial REGEXP %(serial)s
+ORDER BY A.OrderID, B.DateDue ASC)
+UNION
+(SELECT A.OrderID, A.Client, A.RFD, A.ARD, A.AccountManager, A.PO, A.RR, A.RTS, A.Status,
 B.ListID, B.DateOrdered, B.DateDue, B.Remarks
 FROM DemoOrder as A 
 INNER JOIN DemoDuration as B
@@ -9,5 +21,5 @@ INNER JOIN Items as D
 ON C.ItemID = D.ItemID
 INNER JOIN UnitParts as E
 ON D.ItemID = E.ItemID
-WHERE D.Serial REGEXP %(serial)s OR E.Serial REGEXP %(serial)s
-ORDER BY A.OrderID, B.DateDue ASC
+WHERE E.Serial REGEXP %(serial)s
+ORDER BY A.OrderID, B.DateDue ASC)
